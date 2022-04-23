6dc576835ac35c0c0d9f067de7f4bf99795c28c5
Soksedtha Ly <soksedtha.ly@student.mahidol.edu>
2022-04-23 09:13:59 +0700

    Add NavBar to Home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Navbar } from '../components/Navbar'
+import { Footer } from '../components/Footer'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <title>Soksedtha Ly</title>
+        <meta
+          name="description"
+          content="A Portfolio and Blog Site by Soksedtha Ly"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Navbar />
+      <Component {...pageProps} />
+      {/* <Footer /> */}
+    </div>
+  )
 }
 
 export default MyApp
