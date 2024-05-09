2fcc64154c10ff3271696a6de768eee4336fb447
gtmetric <ly.soksedtha81@gmail.com>
2024-05-09 11:37:27 +0800

    Remove unused code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,6 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import { Footer } from '../components/Footer'
-import { Navbar } from '../components/Navbar'
 import About from './about'
 
 const Home: NextPage = About
-// const Home: NextPage = () => {
-//   return (
-//     <main className="text-center py-12 px-8">
-//       <h1 className="text-3xl font-bold py-24">Home</h1>
-//     </main>
-//   )
-// }
 
 export default Home
